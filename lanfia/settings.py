f021cba7bf1484ddba0804d2634d69a057472034
unknown <issa97403@gmail.com>
2022-08-18 03:24:02 +0200

    seconds2

--- lanfia/settings.py
+++ lanfia/settings.py
@@ -197,7 +197,7 @@ CORS_ALLOWED_ORIGINS = [
     "https://localhost:3000",
     "https://127.0.0.1:3000",
     "https://127.0.0.1:8000",
-    "https://dashlanfia.herokuapp.com/"
+    "https://dashlanfia.herokuapp.com/"]
 CORS_ALLOW_METHODS = [
     "DELETE",
     "GET",
