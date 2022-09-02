397ccd8bb964b4228e681b807a63e80dfd4f371f
unknown <issa97403@gmail.com>
2022-09-02 23:44:25 +0200

    de

--- lanfia/settings.py
+++ lanfia/settings.py
@@ -26,8 +26,8 @@ SECRET_KEY = 'django-insecure-plupg3j)r0$q+khc=$8rjfc$q%*(byslhpka^ywe#+2csa$-c-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-#ALLOWED_HOSTS = ['0.0.0.0','localhost', '127.0.0.1',"https://127.0.0.1:3000","https://lanfiamyapi.herokuapp.com","https://apidons.herokuapp.com","https://apivulnerable.herokuapp.com",'https://localhost:3000',"http://127.0.0.1:3000",'http://localhost:3000']
-ALLOWED_HOSTS = ['0.0.0.0',"https://dashblanfia.herokuapp.com/","https://dashlanfia.herokuapp.com/", 'localhost', '127.0.0.1',"https://127.0.0.1:3000","https://lanfiamyapi.herokuapp.com","https://apidons.herokuapp.com",'https://localhost:3000',"http://127.0.0.1:3000",'http://localhost:3000']
+ALLOWED_HOSTS = ['0.0.0.0','localhost', '127.0.0.1',"https://127.0.0.1:3000","https://lanfiamyapi.herokuapp.com","https://apidons.herokuapp.com","https://apivulnerable.herokuapp.com",'https://localhost:3000',"http://127.0.0.1:3000",'http://localhost:3000']
+#ALLOWED_HOSTS = ['0.0.0.0',"https://dashblanfia.herokuapp.com/","https://dashlanfia.herokuapp.com/", 'localhost', '127.0.0.1',"https://127.0.0.1:3000","https://lanfiamyapi.herokuapp.com","https://apidons.herokuapp.com",'https://localhost:3000',"http://127.0.0.1:3000",'http://localhost:3000']
 
 #'https://dashlanfia.herokuapp.com/'
 # Application definition
@@ -132,12 +132,13 @@ REST_FRAMEWORK = {
         'rest_framework.permissions.AllowAny',
 ],
     'DEFAULT_FILTER_BACKENDS':[
-        'django_filters.rest_framework.DjangoFilterBackend'
+        'django_filters.rest_framework.DjangoFilterBackend',  
 ],
 # 'DEFAULT_AUTHENTICATION_CLASSES': [
 #     'rest_framework_simplejwt.authentication.JWTAuthentication'
 #     ],
 }
+
 
 # REST_FRAMEWORK = {
 #     'DEFAULT_PERMISSION_CLASSES': [
@@ -192,24 +193,24 @@ django_heroku.settings(locals())
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 AUTH_USER_MODEL = 'custumer.NewUser'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-# CORS_ALLOWED_ORIGINS = [
-#     "http://localhost:3000",
-#     "http://127.0.0.1:3000",
-#     "http://127.0.0.1:8000",
-#     "https://localhost:3000",
-#     "https://127.0.0.1:3000",
-#     "https://127.0.0.1:8000"]
-
-
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
     "http://127.0.0.1:8000",
     "https://localhost:3000",
     "https://127.0.0.1:3000",
-    "https://127.0.0.1:8000",
-    "https://dashlanfia.herokuapp.com/",
-    "https://dashblanfia.herokuapp.com/"]
+    "https://127.0.0.1:8000"]
+
+
+# CORS_ALLOWED_ORIGINS = [
+#     "http://localhost:3000",
+#     "http://127.0.0.1:3000",
+#     "http://127.0.0.1:8000",
+#     "https://localhost:3000",
+#     "https://127.0.0.1:3000",
+#     "https://127.0.0.1:8000",
+#     "https://dashlanfia.herokuapp.com/",
+#     "https://dashblanfia.herokuapp.com/"]
 
 CORS_ALLOW_METHODS = [
     "DELETE",
