053c8d333b4c600c6a0ff8fc7b11ad27994bb94d
unknown <issa97403@gmail.com>
2022-09-02 22:49:37 +0200

    m

--- lanfia/settings.py
+++ lanfia/settings.py
@@ -134,9 +134,9 @@ REST_FRAMEWORK = {
     'DEFAULT_FILTER_BACKENDS':[
         'django_filters.rest_framework.DjangoFilterBackend'
 ],
-'DEFAULT_AUTHENTICATION_CLASSES': [
-    'rest_framework_simplejwt.authentication.JWTAuthentication'
-    ],
+# 'DEFAULT_AUTHENTICATION_CLASSES': [
+#     'rest_framework_simplejwt.authentication.JWTAuthentication'
+#     ],
 }
 
 # REST_FRAMEWORK = {
