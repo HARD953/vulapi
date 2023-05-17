e0398ba40bd2be66ee9374c4d63529f0c4a397da
issa <issa97403@gmail.com>
2023-05-17 13:41:49 +0200

    nouve5

--- custumer/views.py
+++ custumer/views.py
@@ -43,20 +43,20 @@ class CreateAgent(APIView):
     def post(self,request):
         message='Enregistrement reussi'
         data=self.request.data
-        # if data['is_staff']=="is_staff":
-        #     data['is_staff']=True
-        # else:
-        #     data['is_staff']=False
+        if data['is_staff']=="is_staff":
+            data['is_staff']=True
+        else:
+            data['is_staff']=True
 
-        # if data['is_active']=="is_active":
-        #     data['is_active']=True
-        # else:
-        #     data['is_active']=False
+        if data['is_active']=="is_active":
+            data['is_active']=True
+        else:
+            data['is_active']=True
     
         if data['is_agent']=="is_agent":
             data['is_agent']=True
         else:
-            data['is_agent']=False
+            data['is_agent']=True
 
         data['responsable']=self.request.user.user_name
         
